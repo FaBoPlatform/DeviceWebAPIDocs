a41c370a99cf7838d9900c4e949071e072d2c1ec
akira-sasaki <gclue.akira@gmail.com>
2017-07-25 13:08:07 +0900

    Update sample.

--- sample/robot_sample/controller.js
+++ sample/robot_sample/controller.js
@@ -1,4 +1,7 @@
-var angle = 0;
+var BIAS = 1; // 左右のスピードのバイアス値(ここを調整し左右のモータの速さの調整をする)
+var SPEED = 1; // スピードの初期値
+var now_speed = 0; // 現在のスピードを格納
+
 function preview_start() {
     var imageElement = document.getElementById("image");
     var uri = "http://" + ip + ":" + port + "/gotapi/mediastreamRecording/preview?serviceId=" + hostId;
@@ -15,7 +18,6 @@ function preview_start() {
         } else {
             console.log(json.result);
         }
-
     }, function(errorCode, errorMessage) {
         console.log(errorMessage);
     });
@@ -35,17 +37,48 @@ function preview_stop() {
 }
 
 
-function changeAngle(mAngle) {
-    console.log(mAngle);
-    angle = mAngle;
+function change_angle(angle) {
+    console.log(now_speed);
+    console.log(angle);
+    move(now_speed, angle);
 }
 
-function move(speed) {
+function run_forward() {
+    move(SPEED, 0);
+    now_speed = SPEED;
+}
+
+function run_back() {
+    move(-SPEED, 0);
+    now_speed = -SPEED;
+}
+
+function move(speed, angle) {
     var uri = "http://" + ip + ":" + port + "/gotapi/driveController/move?serviceId=" + mouseId;
+
     var header = null;
     var data = "speed="+speed;
+    angle = Number(angle) + Number(BIAS);
+    if(angle > 360) {
+        angle = 360;
+    } else if(angle < -360) {
+        angle = -360;
+    }
     data += "&angle="+angle;
+    
     dConnect.post(uri, header, data, function(json) {
+        console.log(json.result);
+    }, function(errorCode, errorMessage) {
+        console.log(errorMessage);
+    });
+}
+
+function stop() {
+    var uri = "http://" + ip + ":" + port + "/gotapi/driveController/stop?serviceId=" + mouseId;
+    now_speed = 0;
+    var header = null;
+    var data = null;
+    dConnect.delete(uri, header, data, function(json) {
         console.log(json.result);
     }, function(errorCode, errorMessage) {
         console.log(errorMessage);
